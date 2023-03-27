c30dab259abb6d735c3fb6b65881628999304806
Bini Babu Shini <binibabu9590@gmail.com>
2023-03-27 16:15:47 -0400

    Home

---
+++ src/component/Banner/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Header = () => {
+    return(
+        <nav>
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="menu.html">Menu</a></li>
+          <li><a href="special.html">Special</a></li>
+          <li><a href="Aboutus.html">About us </a></li>
+          <li><a href="Contactus.html">Contact us</a></li>
+  
+        </ul>
+      </nav>
+    )
+}
+
+export  default Header
