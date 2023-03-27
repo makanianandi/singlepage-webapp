360a955db1f33667f366b36247e3f1d3b406d105
Bini Babu Shini <binibabu9590@gmail.com>
2023-03-27 16:28:14 -0400

    Home

---
+++ src/component/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+const Footer = () => {
+    return(
+<div class="footer">
+  <div>
+    <h4>Indian Cuisine | Street Food</h4>
+    <p>
+      Images used from <a href="https://www.pexels.com/photo/homemade-chicken-biryani-4224312/">PIXELS
+        &copy;</a><br />
+    </p>
+  </div>
+  <p>
+    Call: <a href="tel:+1-226-593-9590">+1-226-593-9590</a> <br />
+    Email: <a href="mailto:Indiancuisine@gmail.com">Indiancuisine@gmail.com</a>
+  </p>
+</div>
+)
+}
+
+export  default Footer
---
+++ src/component/Home.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+
+const Home = () => {
+    return(
+        <main>
+        <div class="heroSection">
+          <div class="hero">
+            <h1>India have variety of food</h1>
+    
+          </div>
+        </div>
+        <section>
+          <div class="marquee">
+            <h3>Most Special Food</h3>
+          </div>
+            <div class="grid_container">
+              <div class="grid_item1">
+                <p>Chicken Biryani</p>
+              </div>
+              <div class="grid_item2">
+    
+                <p>Indian Dumplings</p>
+              </div>
+              <div class="grid_item3">
+                <p>Channa Masala</p>
+    
+              </div>
+              <div class="grid_item4">
+    
+                <p>Appam</p>
+              </div>
+    
+              <div class="grid_item5">
+                <p>Allu Pratha</p>
+              </div>
+    
+              <div class="grid_item6">
+                <p>Gol Gappa</p>
+              </div>
+            </div>
+            </section>
+            </main> 
+           
+    )
+}
+
+export default Home
